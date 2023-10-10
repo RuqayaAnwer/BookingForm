b355421e674d872a0a5019f29773fc13167fff2a
RuqayaAnwer <ruqayaanwer5@gmail.com>
2023-10-11 00:30:48 +0300

    first commit

---
+++ BookingForm/BookingForm.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+const BookingForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    date: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data:', formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">الاسم:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">boking@gmail.com</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="date">Date:</label>
+        <input
+          type="date"
+          id="date"
+          name="date"
+          value={formData.date}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div>
+        <button type="submit">Booking taple</button>
+      </div>
+    </form>
+  );
+};
+
+export default BookingForm;
